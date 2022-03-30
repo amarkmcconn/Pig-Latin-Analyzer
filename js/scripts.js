8fe2a1294adf325475bafdeaf0043f32bf5ff436
Mark McConnell <amark.mcconnell@gmail.com>
2022-03-30 13:19:39 -0700

    second vowelvowelManipulator() test complete.
    >  Co-authored-by: James fox < jimmyfox00@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 function vowelManipulator(text) {
-  if (text === "a" || text === "e" || text === "i" || text === "o" || text === "u") {
+  const firstLetter = text.charAt(0);
+  console.log(firstLetter)
+  if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u") {
     return true
   } else
     return false
