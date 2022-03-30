ad7df62aa847f35ae07e09e7bf00185ab59eea02
Mark McConnell <amark.mcconnell@gmail.com>
2022-03-30 13:59:59 -0700

    eighth vowelManipulator() test complete.
    >  Co-authored-by: James fox <jimmyfox00@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 function vowelManipulator(text) {
   const wordArray = text.split(" ")
-  console.log(wordArray)
+  let finalSentence = [];
   wordArray.forEach((word)=>{
     const firstLetter = word.charAt(0).toLowerCase();
     if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u") {
       const wayWord = word + "way"
-      console.log(wayWord)  
-    } else {
-
+      finalSentence.push(wayWord) 
+    }else {
+      finalSentence.push(word)
     }
   })
+  const sentenceString = finalSentence.join(" ");
+  return sentenceString;
 }
   
